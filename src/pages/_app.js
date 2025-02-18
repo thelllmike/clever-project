d05669cbe347f801f4edf6cc2ef7669a882f8373
thelllmike <sachinharshitha179@gmail.com>
2025-02-18 15:56:24 +0530

    add logo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,10 @@
+import Head from "next/head";
 import { AnimatePresence } from "framer-motion";
 import Nav from "../components/Nav";
 import "@/styles/globals.css";
 import Lenis from "lenis";
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
-import Head from "next/head";  // <-- Import Head for SEO meta tags
 
 export default function App({ Component, pageProps, router }) {
   const pathname = usePathname();
@@ -24,9 +24,13 @@ export default function App({ Component, pageProps, router }) {
 
   return (
     <>
-      {/* Google Site Verification Meta Tag */}
       <Head>
+        {/* Google Site Verification */}
         <meta name="google-site-verification" content="4QlFs4MXSaMUGu461bl5U01tLvnfKJWi5yOwh0ha2ew" />
+
+        {/* Favicon Links */}
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </Head>
 
       <Nav />
